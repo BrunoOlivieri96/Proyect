03d18f77ee39ad7b480ca0f2dabe4dc191c848cc
Bruno olivieri <brunolivieri96@gmail.com>
2022-08-17 19:18:56 -0300

    entrega

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,16 @@
+import CartListItem from "../components/CartProcess/CartlistItem"
+
 const Cart = () => {
     return (
-        <span>Pagina cart</span>
-    )
+        <div className="container">
+            <section className='main-container'>
+                <h4>Tu carrito de compras</h4>
+                <CartListItem />
+            </section>        
+        
+        </div>
+
+)
 }
 
 export default Cart
